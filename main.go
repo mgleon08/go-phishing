489e36645e03071e254ae43ce360b39967bf6333
Leon Ji <mgleon08@gmail.com>
2019-02-13 00:00:09 +0800

    basic proxy request to github

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+func handler(w http.ResponseWriter, r *http.Request) {
+	req := cloneRequest(r)
+	body := sendReqToUpstream(req)
+	w.Write(body)
+}
+
+func cloneRequest(r *http.Request) *http.Request {
+	// 取得原請求的 method、body
+	method := r.Method
+	body := r.Body
+
+	// 取得原請求的 url，把它的域名替換成真正的 Github
+	path := r.URL.Path
+	rawQuery := r.URL.RawQuery
+	url := "https://github.com" + path + "?" + rawQuery
+
+	// 建立新的 http.Request
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+func sendReqToUpstream(req *http.Request) []byte {
+	// 建立 http client
+	client := http.Client{}
+
+	// client.Do(req) 會發出請求到 Github、得到回覆 resp
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	// 把回覆的 body 從 Reader（串流）轉成 []byte
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	// res.Body 讀取完，要記得 Close，不然會有 memory leak 等相關問題
+	resp.Body.Close()
+
+	// 回傳 body
+	return respBody
 }
 
 func main() {
