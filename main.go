3ea34e1878a52caa992b8444a33441b2047c4d56
Leon Ji <mgleon08@gmail.com>
2019-02-14 00:07:52 +0800

    replace body url

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
+)
+
+const (
+	upstreamURL = "https://github.com"
+	phishURL    = "http://localhost:8080"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	req := cloneRequest(r)
-	body := sendReqToUpstream(req)
+	// 取得 body & header
+	body, header := sendReqToUpstream(req)
+
+	// 取代後的 body
+	body = replaceURLInResp(body, header)
 	w.Write(body)
 }
 
@@ -20,7 +32,7 @@ func cloneRequest(r *http.Request) *http.Request {
 	// 取得原請求的 url，把它的域名替換成真正的 Github
 	path := r.URL.Path
 	rawQuery := r.URL.RawQuery
-	url := "https://github.com" + path + "?" + rawQuery
+	url := upstreamURL + path + "?" + rawQuery
 
 	// 建立新的 http.Request
 	req, err := http.NewRequest(method, url, body)
@@ -30,7 +42,7 @@ func cloneRequest(r *http.Request) *http.Request {
 	return req
 }
 
-func sendReqToUpstream(req *http.Request) []byte {
+func sendReqToUpstream(req *http.Request) ([]byte, http.Header) {
 	// 建立 http client
 	client := http.Client{}
 
@@ -48,8 +60,38 @@ func sendReqToUpstream(req *http.Request) []byte {
 	// res.Body 讀取完，要記得 Close，不然會有 memory leak 等相關問題
 	resp.Body.Close()
 
-	// 回傳 body
-	return respBody
+	// 回傳 body + header
+	return respBody, resp.Header
+}
+
+func replaceURLInResp(body []byte, header http.Header) []byte {
+	// 判斷 Content-Type 是不是 text/html
+	contentType := header.Get("Content-Type")
+	isHTML := strings.Contains(contentType, "text/html")
+
+	// 如果不是 HTML 就不取代
+	if !isHTML {
+		return body
+	}
+
+	// 把 https://github.com 取代為 http://localhost:8080
+	// strings.Replace 最後一個參數是指最多取代幾個，-1 就是全部都取代
+	bodyStr := string(body)
+	bodyStr = strings.Replace(bodyStr, upstreamURL, phishURL, -1)
+
+	phishGitURL := fmt.Sprintf(`%s(.*)\.git`, phishURL)
+	upstreamGitURL := fmt.Sprintf(`%s$1.git`, upstreamURL)
+
+	// 尋找符合 git 網址的特徵
+	re, err := regexp.Compile(phishGitURL)
+	if err != nil {
+		panic(err)
+	}
+
+	// 取代成 github 網址
+	bodyStr = re.ReplaceAllString(bodyStr, upstreamGitURL)
+
+	return []byte(bodyStr)
 }
 
 func main() {
